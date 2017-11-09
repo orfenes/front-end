9f46e9a75c7e3c8f5bc14710d2609bd600a37b93
Rodrigo Castro <rfocastro@sp.r7.com>
2017-11-09 18:10:53 -0200

    criando componentes e rotas react

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
   <div>
-    <h1>Sobre</h1>
+    <PageHeader name='Sobre' small='Nós'></PageHeader>
+    <h2>Nossa História</h2>
+    <p>Lorem ipsum dolor sit amet...</p>
+    <h2>Missao e valor</h2>
+    <p>Lorem ipsum...</p>
+    <h2>Imprensa</h2>
+    <p>Lorem Ipsum...</p>
   </div>
 )
---
+++ src/todo/todoList.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default props => (
+  <div>
+    <h2>Listsa</h2>
+  </div>
+)
