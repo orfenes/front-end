2a22ca57b70d232e8a2944b13e5e4d13e8bf8ed1
Rodrigo Castro <rfocastro@sp.r7.com>
2017-11-13 14:59:43 -0200

    configurando component events

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,14 +1,38 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
 
+const URL = 'http://localhost:3003/api/todos'
+
+
 export default class Todo extends Component{
+
+  constructor(props){
+    super(props)
+    this.state = { description: '', list: [] }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleAdd = this.handleAdd.bind(this)    
+  }
+
+  handleChange(e){
+    this.setState({...this.state, description: e.target.value })
+  }
+
+  handleAdd(){
+    const description = this.state.description
+    console.log('chamando recutso', description)
+    axios.post(URL, {description})
+      .then(resp => console.log('Funcionou!'))       
+  }
+
   render(){
     return(
       <div>
         <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-        <TodoForm />
+        <TodoForm handleAdd={this.handleAdd} description={this.state.description} handleChange={this.handleChange}/>
         <TodoList />
       </div>
     )
