6a4df23413ec3aa09a148e0ac4237dae3909999b
Rodrigo Castro <rfocastro@sp.r7.com>
2017-12-28 18:33:51 -0200

    finalizando primeira estrutura

--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -1,7 +1,51 @@
 import React from 'react'
+import IconButton from '../template/iconButton'
 
-export default props => (
-  <div>
-    <h2>Listsa</h2>
-  </div>
-)
+export default props => {
+
+  const renderRows = () => {
+    const list = props.list || [];
+    return list.map((todo) =>(
+      <tr key={todo._id}>
+        <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
+        <td>
+          <IconButton 
+            style='success' 
+            icon='check' 
+            hide={todo.done}
+            onClick={() => props.handleMarkAsDone(todo)}>
+          </IconButton>
+
+          <IconButton 
+            style='warning' 
+            icon='undo' 
+            hide={!todo.done}
+            onClick={() => props.handleMarkAsPending(todo)}>
+          </IconButton>
+
+          <IconButton 
+            style='danger' 
+            icon='trash' 
+            hide={!todo.done}
+            onClick={() => props.handleRemove(todo)}>
+          </IconButton>
+        </td>
+      </tr>
+    ))
+  }
+
+  return(
+    <table className='table'>
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <th className='tableActions'> Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        {renderRows()}
+      </tbody>      
+    </table>
+  )
+
+}
